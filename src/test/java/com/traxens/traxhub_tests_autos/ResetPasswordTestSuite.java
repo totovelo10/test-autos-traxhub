3ce81cf5f1ede8eb5f373484acaa6854cf4a69f8
sboina <sboina@DES-SI-SBO.traxens.local>
2019-08-05 16:51:25 +0200

    feature: reset password uncommented

--- src/test/java/com/traxens/traxhub_tests_autos/ResetPasswordTestSuite.java
+++ src/test/java/com/traxens/traxhub_tests_autos/ResetPasswordTestSuite.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-/*
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/features/authentication/ResetPassword.feature")
 public class ResetPasswordTestSuite {}
-*/
